dece0c76ad7ab794b1e3f2ea77f975f54ce17eb2
NadiaTeta <144005791+NadiaTeta@users.noreply.github.com>
2024-05-19 13:41:15 +0200

    Update UniqueInt.py

--- UniqueInt.py
+++ UniqueInt.py
@@ -1,1 +1,13 @@
-import os 
+#!/usr/bin/python3
+with open("sample_input_for_students/sample_01", "r") as x:
+  lines = x.readlines()
+
+numbers = [ int(line.strip()) for line in lines ]
+
+sorted_numbers = sorted(numbers)
+
+unique = list(set(sorted_numbers))
+
+with open("results_for_sample_inputs/sample_01.txt_result", "a") as n:
+  for i in unique:
+      n.write(str(i) + "\n")
