4c5dc4872c6cbd0ddbe24d74e51779e641fb39f2
NadiaTeta <n.teta2@alustudent.com>
2024-05-20 13:41:45 +0200

    d

--- UniqueInt.py
+++ UniqueInt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-with open("sample_input_for_students/sample_01", "r") as x:
+with open(input_file_path, "r") as x:
   lines = x.readlines()
 
 numbers = [ int(line.strip()) for line in lines ]
@@ -8,6 +8,6 @@ sorted_numbers = sorted(numbers)
 
 unique = list(set(sorted_numbers))
 
-with open("results_for_sample_inputs/sample_01.txt_result", "a") as n:
+with open("results.txt", "a") as n:
   for i in unique:
       n.write(str(i) + "\n")
